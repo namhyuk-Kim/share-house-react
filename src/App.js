b2a6d61d7affdb0a669a94bee7a2dc426c722151
laon-dev <laon.dev1@gmail.com>
2019-11-17 20:45:22 +0900

    .

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from "react";
 import MainTemplate from "./components/base/MainTemplate/MainTemplate";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Main from "components/common/Main/Main";
+import Register from "components/common/Register/Register";
 import "./App.css";
 
 class App extends React.Component {
   render() {
-    return <MainTemplate />;
+    return (
+      <BrowserRouter>
+        <MainTemplate>
+          <Route exact path="/" component={Main}></Route>
+          <Route exact path="/Register" component={Register}></Route>
+          {/* <Route exact path="/login" component={Login}></Route> */}
+        </MainTemplate>
+      </BrowserRouter>
+    );
   }
 }
 
